9043c8e2f960e4dbdf8a5a8a3df1e7de29c6b754
Mesum Ali <syedmesumjaffary@gmail.com>
2025-05-07 15:08:15 +0500

    add Qr

--- decode.py
+++
@@ -1,9 +0,0 @@
-from pyzbar.pyzbar import decode
-
-from PIL import Image
-
-img = Image.open("C:/Mesum/Python/GIAIC_Projects/Project_5_Assignments/Assignments 01/03_advanced/Project_4_QR_Code_Encoder_and_Decoder/myqrcode.png")
-
-result = decode(img)
-
-print(result)
